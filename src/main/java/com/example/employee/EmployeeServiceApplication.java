1784210f32c1c3d537ad86d6fa10ff80d7b7b8be
suni1234 <sunilkumar.kakani11@gmail.com>
2026-07-11 22:55:42 -0400

    adding line to check jenki's trigger build

--- src/main/java/com/example/employee/EmployeeServiceApplication.java
+++ src/main/java/com/example/employee/EmployeeServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //Employee Service - CRUD REST API - EMP-101
 public class EmployeeServiceApplication {
     public static void main(String[] args) {
+    	System.out.println("helllo");
         SpringApplication.run(EmployeeServiceApplication.class, args);
     }
 }
